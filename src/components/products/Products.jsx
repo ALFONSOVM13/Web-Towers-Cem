e575318c8642fdf5685aab48fa03ce81e9123168
Jesús Sánchez <jmsr060391@gmail.com>
2024-06-30 08:55:45 -0500

    TC-13

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -14,7 +14,7 @@ const Products = () => {
           height={400}
           alt="Cemento uso general"
         />
-        <SliderButton className={'mx-auto w-auto space-y-4'}/>
+        <SliderButton className={'mx-auto w-auto space-y-4'} />
       </div>
       <div className='flex-col space-y-8 flex-1'>
         <Title
@@ -23,7 +23,7 @@ const Products = () => {
           size={'large'}
         />
         <Title
-          text="CEMENTO USO GENERAL"
+          text="USO GENERAL"
           className={'font-title font-bold'}
         />
         <p className='text-complementary-300 font-title text-xl'>Nuestro cemento X-Cem utiliza tecnología avanzada para proporcionar altos niveles de durabilidad y sostenibilidad. Descubre cómo X-Cem puede transformar tus proyectos de construcción.</p>
@@ -49,3 +49,108 @@ const Products = () => {
   )
 }
 export default Products
+
+
+// import React, { useState } from "react";
+// import Image from 'next/image';
+// import Title from '../ui/Title';
+// import CustomButton from '../ui/CustomButton';
+// import SliderButton from '../ui/SliderButton';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { EffectFlip, Pagination, Navigation } from 'swiper/modules';
+// import 'swiper/css';
+// import 'swiper/css/effect-flip';
+// import 'swiper/css/pagination';
+// import 'swiper/css/navigation';
+
+// const productData = [
+//   {
+//     image: URL="/public/images/cemento.svg",
+//     title: "X-CEM",
+//     subtitle: "USO GENERAL",
+//     description: "Nuestro cemento X-Cem utiliza tecnología avanzada para proporcionar altos niveles de durabilidad y sostenibilidad. Descubre cómo X-Cem puede transformar tus proyectos de construcción."
+//   },
+//   {
+//     image: URL="/public/images/bolsa-blanca-42.5.png",
+//     title: "X-CEM",
+//     subtitle: "USO ESTRUCTURAL",
+//     description: "Revolucionamos los estándares de la construcción, minimizamos el impacto en el ecosistema y maximizamos la eficiencia de los recursos. Cada tonelada (t) de cemento X-cem remueve hasta 173.35 Kg de CO2 de la atmósfera."
+//   },
+//   {
+//     image: URL="/public/images/bolsa-42.50kg-marron.png",
+//     title: "X-CEM",
+//     subtitle: "USO ESTRUCTURAL",
+//     description: "Revolucionamos los estándares de la construcción, minimizamos el impacto en el ecosistema y maximizamos la eficiencia de los recursos. Cada tonelada (t) de cemento X-cem remueve hasta 173.35 Kg de CO2 de la atmósfera."
+//   },
+// ];
+
+//   const Products = () => {
+//   const [activeProduct, setActiveProduct] = useState(0);
+
+//   const handleSlideChange = (swiper) => {
+//     setActiveProduct(swiper.activeIndex);
+//   };
+
+//   return (
+//     <div className='flex p-14 justify-center items-center'>
+//       <div className='flex flex-col flex-2 ml-28'>
+//         <Swiper
+//           effect={'flip'}
+//           grabCursor={true}
+//           pagination={true}
+//           navigation={true}
+//           modules={[EffectFlip, Pagination, Navigation]}
+//           className="mySwiper"
+//           onSlideChange={handleSlideChange}
+//         >
+//           {productData.map((product, index) => (
+//             <SwiperSlide key={index}>
+//               <div style={{ width: 400, height: 400, position: 'relative' }}>
+//                 <Image
+//                   src={product.image}
+//                   layout="fill"
+//                   objectFit="cover"
+//                   alt={product.title}
+//                 />
+//               </div>
+//             </SwiperSlide>
+//           ))}
+//         </Swiper>
+//         <SliderButton className={'mx-auto w-auto space-y-4'} />
+//       </div>
+//       <div className='flex-col space-y-8 flex-1'>
+//         <Title
+//           text={productData[activeProduct].title}
+//           className={'font-title font-bold text-primary-100'}
+//           size={'large'}
+//         />
+//         <Title
+//           text={productData[activeProduct].subtitle}
+//           className={'font-title font-bold'}
+//         />
+//         <p className='text-complementary-300 font-title text-xl'>
+//           {productData[activeProduct].description}
+//         </p>
+
+//         <div className='flex gap-6 mt-10'>
+//           <CustomButton
+//             type='primary'
+//             onClick={() => {}}
+//             className={'rounded-2xl'}
+//           >
+//             VER FICHA TECNICA
+//           </CustomButton>
+//           <CustomButton
+//             type='secundary'
+//             onClick={() => {}}
+//             className={'rounded-2xl'}
+//           >
+//             SOLICITAR MUESTRAS
+//           </CustomButton>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Products;
--- src/components/ui/CustomButton.jsx
+++ src/components/ui/CustomButton.jsx
@@ -8,7 +8,7 @@ const CustomButton = ({ type = 'primary', onClick, children, className }) => {
   // Estilos para el botón primario y secundario
   const typeClasses = type === 'primary'
     ? 'bg-primary-100 text-white hover:bg-primary-200'
-    : 'border border-primary-100 text-primary-100 hover:bg-complementary-100 hover:text-white color transition-colors duration-500';
+    : 'border border-primary-100 text-primary-100 hover:bg-primary-100 hover:text-white color transition-colors duration-500';
 
   return (
     <button
