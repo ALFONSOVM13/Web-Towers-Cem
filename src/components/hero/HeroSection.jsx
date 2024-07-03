152fbf5bb4f5c47d56e7040ff58bbf1c224cf1c3
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-07-03 12:02:48 -0500

    correccion responsive

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -4,28 +4,27 @@ import CustomButton from "../ui/CustomButton";
 
 const HeroSection = () => {
   return (
-    <div
-      className="relative w-full h-screen bg-cover bg-center overflow-hidden"
-      style={{ backgroundImage: "url('/images/hero-background.jpg')" }}
-    >
+    <div className="relative w-full h-screen bg-cover bg-center overflow-hidden"
+      style={{ backgroundImage: "url('/images/hero-background.jpg')" }}>
+
       <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-90"></div>
 
-      <div className="absolute inset-0 flex items-center justify-center text-center text-complementary-200">
-        <div className="max-w-4xl px-6 md:px-10 lg:px-20 ">
-          <Title
+      <div className="absolute inset-0 flex items-center justify-center text-center">
+        <div className="max-w-4xl px-6 md:px-10 lg:px-20">
+          <Title 
             text="CONSTRUYENDO CIUDADES QUE RESPIRAN"
             size="large"
             color="white"
-            className={"animate-jump-in"}
+            className="animate-jump-in text-shadow-smooth mb-6 text-complementary-200"
           />
-          <p className="mt-4 text-lg md:text-2xl leading-relaxed font-title">
+          <p className="text-xl md:text-2xl leading-relaxed font-content text-white mb-8">
             Innovación y sostenibilidad se unen para construir el futuro con
             cementos inteligentes y ecológicos.
           </p>
+          <CustomButton text="Descubre más" /> 
         </div>
       </div>
     </div>
-      
   );
 };
 
