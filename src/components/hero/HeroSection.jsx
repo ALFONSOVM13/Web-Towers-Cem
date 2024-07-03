68336da5ede038ea5ec08b70726d3d9202745d5b
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-07-03 12:24:59 -0500

    animacion

--- src/components/hero/HeroSection.jsx
+++ src/components/hero/HeroSection.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Title from "../ui/Title";
-import CustomButton from "../ui/CustomButton";
-
+import { PiMouseSimple } from "react-icons/pi";
+import { FaAnglesDown } from "react-icons/fa6";
 const HeroSection = () => {
   return (
-    <div className="relative w-full h-screen bg-cover bg-center overflow-hidden"
-      style={{ backgroundImage: "url('/images/hero-background.jpg')" }}>
-
+    <div
+      className="relative w-full h-screen bg-cover bg-center overflow-hidden"
+      style={{ backgroundImage: "url('/images/hero-background.jpg')" }}
+    >
       <div className="absolute inset-0 bg-gradient-to-b from-black to-transparent opacity-90"></div>
 
       <div className="absolute inset-0 flex items-center justify-center text-center">
         <div className="max-w-4xl px-6 md:px-10 lg:px-20">
-          <Title 
+          <Title
             text="CONSTRUYENDO CIUDADES QUE RESPIRAN"
             size="large"
             color="white"
@@ -21,7 +22,12 @@ const HeroSection = () => {
             Innovación y sostenibilidad se unen para construir el futuro con
             cementos inteligentes y ecológicos.
           </p>
-          <CustomButton text="Descubre más" /> 
+          <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2">
+            <div className="flex items-center flex-col justify-center animate-bounce animate-infinite animate-ease-in-out">
+              <PiMouseSimple className=" text-white text-5xl" />
+              <FaAnglesDown className="text-white text-3xl mt-4 " />
+            </div>
+          </div>
         </div>
       </div>
     </div>
