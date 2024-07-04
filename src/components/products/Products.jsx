9cedde0c82a52df68fff02453db9ff5563e55ea1
Jesús Sánchez <jmsr060391@gmail.com>
2024-07-04 13:57:36 -0500

    agregandoAdvantages

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,12 +1,13 @@
-import React, { useCallback, useRef } from "react";
-import Image from "next/image";
-import Title from "../ui/Title";
-import CustomButton from "../ui/CustomButton";
-import SliderButton from "../ui/SliderButton";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { FreeMode, EffectFade } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/effect-fade";
+import React, { useCallback, useRef } from "react"
+import Image from "next/image"
+import Title from "../ui/Title"
+import CustomButton from "../ui/CustomButton"
+import SliderButton from "../ui/SliderButton"
+import { Swiper, SwiperSlide } from "swiper/react"
+import { FreeMode, EffectFade } from "swiper/modules"
+import "swiper/css"
+import "swiper/css/effect-fade"
+import Advantages from './Advantages'
 
 const products = [
   {
@@ -33,22 +34,22 @@ const products = [
     description:
       "Revolucionamos los estándares de la construcción, minimizamos el impacto en el ecosistema y maximizamos la eficiencia de los recursos. Cada tonelada (t) de cemento X-cem remueve hasta 173.35 Kg de CO2 de la atmósfera.",
   },
-];
+]
 
 const Products = () => {
-  const mainSliderRef = useRef(null);
+  const mainSliderRef = useRef(null)
 
   const handleSlidePrev = useCallback(() => {
     if (mainSliderRef.current) {
-      mainSliderRef.current.swiper.slidePrev();
+      mainSliderRef.current.swiper.slidePrev()
     }
-  }, []);
+  }, [])
 
   const handleSlideNext = useCallback(() => {
     if (mainSliderRef.current) {
-      mainSliderRef.current.swiper.slideNext();
+      mainSliderRef.current.swiper.slideNext()
     }
-  }, []);
+  }, [])
 
   return (
     <section className="bg-complementary-200">
@@ -60,17 +61,17 @@ const Products = () => {
         modules={[FreeMode, EffectFade]}
         className="h-full"
       >
-        
+
         {products.map((product) => (
           <SwiperSlide className="bg-complementary-200" key={product.id}>
-            
+
             <div className="flex flex-col md:flex-col p-6 md:p-14 justify-center items-center min-h-screen">
-            <Title text="NUESTROS PRODUCTOS" size="large" className="text-primary-100 text-center my-10" />
+              <Title text="NUESTROS PRODUCTOS" size="large" className="text-primary-100 text-center my-10" />
 
               <div className="w-full max-w-6xl mx-auto flex flex-col md:flex-row justify-center items-center">
-                
+
                 <div className="flex flex-col flex-2 md:ml-10 items-center md:items-center gap-4">
-                  
+
                   <figure className="min-h-[400px] h-[400px]">
                     <Image
                       src={product.image}
@@ -103,14 +104,14 @@ const Products = () => {
                   <div className="flex flex-col lg:flex-row gap-6 mt-10">
                     <CustomButton
                       type="primary"
-                      onClick={() => {}}
+                      onClick={() => { }}
                       className="rounded-2xl"
                     >
                       VER FICHA TECNICA
                     </CustomButton>
                     <CustomButton
                       type="secundary"
-                      onClick={() => {}}
+                      onClick={() => { }}
                       className="rounded-2xl"
                     >
                       SOLICITAR MUESTRAS
@@ -122,8 +123,10 @@ const Products = () => {
           </SwiperSlide>
         ))}
       </Swiper>
-    </section>
-  );
-};
 
-export default Products;
+      <Advantages />
+    </section>
+  )
+}
+
+export default Products
