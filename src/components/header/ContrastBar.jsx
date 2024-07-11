4fb11cfc3ed4c88f293d08fa0b76e1a2201b3adb
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-07-11 13:54:13 -0600

    TC-17 Avance 1

---
+++ src/components/header/ContrastBar.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContrastBar = () => {
+  return (
+    <div className="fixed h-32 w-screen bg-[#333333] top-0 left-0 z-[10]"></div>
+  );
+};
+
+export default ContrastBar;
