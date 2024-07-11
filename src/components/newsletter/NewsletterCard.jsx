5076af2cc30e86058a1f090f57849793021e6d52
Luis Alberto Camarillo Flores <luiluego@gmail.com>
2024-07-11 16:03:13 -0600

    TC-17 Avance 2

--- src/components/newsletter/NewsletterCard.jsx
+++ src/components/newsletter/NewsletterCard.jsx
@@ -1,10 +1,20 @@
+"use client";
 import React from "react";
+import { useState } from "react";
 
 const NewsletterCard = ({ image, title, readTime, index }) => {
+  const [isOvered, setIsOvered] = useState(false);
+
   return (
-    <div className={`relative w-full h-[400px] rounded-md overflow-hidden`}>
+    <div
+      className={`relative w-full h-[400px] rounded-md overflow-hidden`}
+      onMouseEnter={() => setIsOvered(true)}
+      onMouseLeave={() => setIsOvered(false)}
+    >
       <div
-        className={`w-full h-full bg-cover bg-center scale-105 hover:scale-110 hover:grayscale-[5%] transition-all duration-500 grayscale-[75%]`}
+        className={`w-full h-full bg-cover bg-center ${
+          isOvered ? "scale-110 grayscale-[5%]" : "scale-105 grayscale-[75%]"
+        } transition-all duration-500`}
         style={{ backgroundImage: `url('${image}')` }}
       ></div>
       <div className="flex flex-col p-5 absolute bottom-0 gap-2 font-bold bg-black/30 w-full backdrop-blur-sm">
