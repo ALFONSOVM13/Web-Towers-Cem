2f4a3cc9cc8d1d1847df3a85f92f909471b7307a
tomy1375 <tomy_ramos1991@yahoo.com.ar>
2024-07-13 20:55:04 -0300

    TC-18 <contactanosComponente>

---
+++ src/components/contact/ContactSection.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ContactSection = () => {
+    return (
+      <>
+        <section >
+         <h1>hola</h1>
+        </section>
+      </>
+    );
+  };
+  
+  export default ContactSection;
