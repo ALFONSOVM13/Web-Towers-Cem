8c406bb0ff436802d1ab129bf4c5431067b02458
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2024-07-26 13:45:44 -0600

    Vista de noticias, actualizada

--- src/components/newsPage/SectionHeader.jsx
+++ src/components/newsPage/SectionHeader.jsx
@@ -3,11 +3,14 @@ import React from "react";
 function SectionHeader({ title, className = "" }) {
   return (
     <header
-      className={`flex gap-5 justify-between text-zinc-900 max-md:flex-wrap max-md:max-w-full ${className}`}
+      className={`flex flex-col max-md:flex-wrap max-md:max-w-full ${className}`}
     >
-      <h1 className="text-6xl font-title tracking-wide leading-[68.2px] max-md:text-4xl text-white">
+      <h1 className="text-6xl font-title tracking-wide leading-[68.2px] max-md:text-4xl font-bold text-complementary-200">
         {title}
       </h1>
+      <h2 className="flex flex-col font-title text-xl text-left mt-2">
+        Towers Cem y el Mundo
+      </h2>
     </header>
   );
 }
