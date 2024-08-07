5dde5116a7f1503b097f69509d9f83e670ffe9a0
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-08-07 18:11:15 -0500

    responsive

--- src/components/knowUs/KnowUsSection.jsx
+++ src/components/knowUs/KnowUsSection.jsx
@@ -5,8 +5,8 @@ import { useInView } from "react-intersection-observer";
 
 const KnowUsSection = () => {
   const { ref, inView } = useInView({
-    triggerOnce: true, 
-    threshold: 0.1, 
+    triggerOnce: true,
+    threshold: 0.1,
   });
 
   return (
@@ -21,15 +21,16 @@ const KnowUsSection = () => {
           <p className="text-complementary-300 font-title text-lg md:text-xl lg:text-2xl text-left leading-8 md:max-w-[85%]">
             Somos una cementera innovadora dedicada a la producción de cementos
             verdes. Utilizando tecnologías avanzadas como{" "}
-            <span className="font-bold">X-CEM</span>,{" "}incorporamos materiales
-            reciclados y propiedades inteligentes en nuestros productos,
-            <span className="font-bold">{" "}permitiendo la absorción de CO₂</span> y
-            contribuyendo a la lucha contra el cambio climático.
+            <span className="font-bold">X-CEM</span>,{" "}
+            incorporamos materiales reciclados y propiedades inteligentes en
+            nuestros productos,{" "}
+            <span className="font-bold">permitiendo la absorción de CO₂</span>{" "}
+            y contribuyendo a la lucha contra el cambio climático.
           </p>
         </div>
         <div className="min-h-[400px] h-full flex items-center justify-center px-4 w-full">
           <div className="relative w-[260px] h-[200px] sm:w-[350px] lg:w-[420px] 2xl:w-[500px] sm:h-[300px] lg:h-[340px] xl:h-[400px]">
-            <figure className="absolute -top-32 sm:top-0 left-0 w-full h-full rounded-sm overflow-hidden">
+            <figure className="absolute -top-28 sm:top-0 left-0 w-full h-full rounded-sm overflow-hidden">
               <Image
                 src="/images/header.png"
                 alt="Imagen de la empresa"
@@ -38,7 +39,7 @@ const KnowUsSection = () => {
                 className="object-cover object-center w-full h-full shadow-lg"
               />
             </figure>
-            <figure className="w-20 h-20 absolute -top-36 -right-5 sm:-top-5 sm:-right-5 ">
+            <figure className="hidden w-20 h-20 absolute -top-36 -right-5 sm:block sm:-top-5 sm:-right-5">
               <Image
                 src="/images/about-us/towerscem_icon-round.png"
                 alt="TowerCem logo"
@@ -47,7 +48,7 @@ const KnowUsSection = () => {
                 className="w-full h-full object-cover object-center"
               />
             </figure>
-            <figure className="absolute bottom-0 left-[50%] -translate-x-[50%] right-auto sm:translate-x-0 sm:left-auto sm:-right-[20%] md:-right-[20%] xl:-right-[32%] 2xl:-right-[25%] border-8 border-y-complementary-400 rounded-[45px] w-[200px] lg:w-[250px]">
+            <figure className="hidden absolute bottom-0 left-[50%] -translate-x-[50%] right-auto sm:block sm:translate-x-0 sm:left-auto sm:-right-[20%] md:-right-[20%] xl:-right-[32%] 2xl:-right-[25%] border-8 border-y-complementary-400 rounded-[45px] w-[200px] lg:w-[250px]">
               <Image
                 src="/images/about-us/TC-CREATIVOS-1.png"
                 alt="TowerCem creativos"
@@ -61,7 +62,7 @@ const KnowUsSection = () => {
             </figure>
             <div
               ref={ref}
-              className="absolute -bottom-[160px] sm:-bottom-20 sm:-left-40 bg-secondary-100 rounded-xl px-6 py-4 xl:px-8 lg:py-5"
+              className="absolute -bottom-[40px] sm:-bottom-20 sm:-left-40 bg-secondary-100 rounded-xl px-6 py-4 xl:px-8 lg:py-5"
             >
               <div className="flex gap-2 sm:gap-2 sm:flex-col">
                 <span className="text-2xl md:text-3xl xl:text-5xl font-bold">
