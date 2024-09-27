f82e2a544d7e6f948cc636ee655b56a1dc45db6a
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-09-26 23:08:07 -0500

    Refactorizar componente Header y actualizar metadatos de la página de cotización

--- src/app/(website)/cotizar/page.jsx
+++ src/app/(website)/cotizar/page.jsx
@@ -1,17 +1,17 @@
 import QuoteSection from '@/components/quotesection/QuoteSection';
 
 export const metadata = {
-  title: "Contacto - Towers Cem",
-  description: "Somos una cementera innovadora dedicada a la producción de cementos verdes, enfocados en la sostenibilidad y la reducción del impacto ambiental. Utilizando tecnologías avanzadas como X-CEM, donde incorporamos materiales reciclados y propiedades inteligentes en nuestros productos, permitiendo la descomposición de CO₂ y contribuyendo a la lucha contra el cambio climático.",
-  image: "/logo.png",
-  favicon: "/src/app/icon.ico",
-  color: "#000000",
-  lang: "es",
-  type: "website",
-  url: "https://towerscem.com",
-  siteName: "Towers Cem",
-  keywords: "towerscem, contacto, cemento, construcción, infraestructuras, sostenibilidad, calidad",
-};
+    title: "Cotizar - Towers Cem | Cementos verdes para un futuro sostenible",
+    description: "Somos una cementera innovadora dedicada a la producción de cementos verdes y sostenibles. Solicita una cotización para productos que integran materiales reciclados y ayudan a reducir el CO₂ en el ambiente.",
+    image: "/logo.png",
+    favicon: "/src/app/icon.ico",
+    color: "#000000",
+    lang: "es",
+    type: "website",
+    url: "https://towerscem.com/cotizar",
+    siteName: "Towers Cem",
+    keywords: "towerscem, contacto, cemento, construcción, infraestructuras, sostenibilidad, cotización, calidad, materiales reciclados",
+  };
 
 const page = () => {
   return (
