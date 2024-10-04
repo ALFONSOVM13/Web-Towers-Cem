c9d0e90276d21a3d73d6761b6ca2ef39e950970d
alfonsovm13 <alfonsovengoechea@gmail.com>
2024-10-04 11:17:13 -0500

    Refactorizar componente NewsSection: ordenar y seleccionar las últimas 4 noticias

--- src/components/newsPreview/NewsSection.jsx
+++ src/components/newsPreview/NewsSection.jsx
@@ -6,10 +6,14 @@ import NewsGrid from "./NewsGrid";
 import news from "../../../public/tempData/news.json";
 
 const NewsSection = () => {
-  const latestNews = news.slice(-4);
+  // Ordenar las noticias por fecha más reciente
+  const sortedNews = news.sort((a, b) => new Date(b.publishedAt) - new Date(a.publishedAt));
+
+  // Seleccionar las últimas 4 noticias
+  const latestNews = sortedNews.slice(0, 4);
 
   return (
-    <section className="mx-12 sm:mx-20  bg-white h-full">
+    <section className="mx-12 sm:mx-20 bg-white h-full">
       <div className="flex justify-between items-center mb-10">
         <Title text="Noticias" />
         <Link href="/noticias" alt="Noticias">
@@ -17,7 +21,7 @@ const NewsSection = () => {
             type="secondary"
             className="flex items-center gap-x-2 rounded-3xl"
           >
-            Ver mas <FaArrowRightLong />
+            Ver más <FaArrowRightLong />
           </CustomButton>
         </Link>
       </div>
