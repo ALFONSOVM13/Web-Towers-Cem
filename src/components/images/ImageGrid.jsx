5257fcb41c03b07ce6ad3c5235fd371b9b490655
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-01-26 23:58:34 -0600

    Grid e Items de Modal de imagenes, añadidos

--- src/components/images/ImageGrid.jsx
+++ src/components/images/ImageGrid.jsx
@@ -4,17 +4,19 @@ import { includesImage } from "@/utils/image"
 
 export const ImageGrid = ({ images, selectedImages, onChangeSelectedImages }) => {
     return (
-        <div className="mt-3 w-full grid grid-cols-[repeat(auto-fill,minmax(6rem,1fr))] gap-4 px-2">
-            {
-                images.map(image => (
-                    <ImageGridItem
-                        key={image.id}
-                        image={image}
-                        isSelectedImage={includesImage( image, selectedImages )}
-                        onChangeSelectedImages={onChangeSelectedImages}
-                    />
-                ))
-            }
+        <div className="md:h-[28rem]">
+            <div className="my-3 w-full grid grid-cols-[repeat(auto-fill,minmax(7rem,1fr))] gap-4 px-2">
+                {
+                    images.map(image => (
+                        <ImageGridItem
+                            key={image.id}
+                            image={image}
+                            isSelectedImage={includesImage( image, selectedImages )}
+                            onChangeSelectedImages={onChangeSelectedImages}
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
--- src/components/images/ImageGridItem.jsx
+++ src/components/images/ImageGridItem.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { IoMdCheckmark } from "react-icons/io";
 
 export const ImageGridItem = ({ image, isSelectedImage, onChangeSelectedImages  }) => {
     return (
@@ -6,7 +7,7 @@ export const ImageGridItem = ({ image, isSelectedImage, onChangeSelectedImages  
             onClick={() => onChangeSelectedImages(image)}
             className={`relative flex flex-col rounded border transition cursor-pointer`}
         >
-            <div className={`relative h-24 aspect-square ${isSelectedImage ? 'opacity-80 scale-90 bg-primary-300' : 'bg-slate-100'}`}>
+            <div className={`relative h-24 aspect-square ${isSelectedImage ? 'opacity-90 scale-90 bg-primary-300' : 'bg-slate-100'}`}>
                 <Image
                     src={image.url}
                     alt={image.originalName}
@@ -17,11 +18,10 @@ export const ImageGridItem = ({ image, isSelectedImage, onChangeSelectedImages  
                 />
             </div>
             <p className="text-xs whitespace-nowrap overflow-hidden text-ellipsis py-1">{image.originalName}</p>
-            <div>
-
-            </div>
             <div className="absolute -top-1 -left-1">
-                <div className={`w-4 h-4 rounded-md shadow ${ isSelectedImage ? 'bg-primary-200' : 'bg-complementary-200 border' }`}></div>
+                <div className={`w-4 h-4 rounded-md shadow flex justify-center items-center ${ isSelectedImage ? 'bg-primary-200' : 'bg-complementary-200 border' }`}>
+                    { isSelectedImage && <IoMdCheckmark className="w-3 h-3 text-white" /> }
+                </div>
             </div>
         </div>
     )
