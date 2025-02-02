6e258a6adfd5336d73bf13574233fba60b0e371a
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-02 14:13:38 -0600

    Posición al centro del contenido del Modal, actualizado

--- src/components/ui/ModalContainer.jsx
+++ src/components/ui/ModalContainer.jsx
@@ -1,18 +1,18 @@
 'use client'
 import { useEffect } from "react"
 
-export const ModalContainer = ({ children, show, onClose }) => {
+export const ModalContainer = ({ children, show, onClose, className="" }) => {
   
   useEffect(() => {
     const body = document.querySelector('body')
     if( show ){
-      body.classList.add('fixed-body')
+      body.classList.add('overflow-hidden')
     }else {
-      body.classList.remove('fixed-body')
+      body.classList.remove('overflow-hidden')
     }
 
   return () => {
-    body.classList.remove('fixed-body')
+    body.classList.remove('overflow-hidden')
   }
 }, [show])
 
@@ -21,13 +21,13 @@ export const ModalContainer = ({ children, show, onClose }) => {
 
   return (
     <div
-      className="fixed inset-0 h-screen max-h-screen overflow-hidden flex justify-center items-center z-50"
+      className="fixed inset-0 overflow-hidden flex justify-center items-center z-50"
     >
       <div 
         onClick={ onClose } 
         className="fixed inset-0 bg-black/50 z-40 animate-fade animate-duration-300">
       </div>
-      <div className="z-50 bg-white rounded p-1 animate-fade-down animate-duration-300">
+      <div className={`z-50 bg-white rounded p-1 animate-fade-down animate-duration-300 ${className}`}>
         { children }
       </div>
     </div>
