f603c883e71d564ba8df669167e25734c5155a01
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-04 14:50:45 -0600

    EventList añadido a page de eventos

--- src/app/admin/eventos/page.jsx
+++ src/app/admin/eventos/page.jsx
@@ -1,16 +1,28 @@
+import { notFound } from 'next/navigation'
 import { AddEventButtom } from '@/components/events/AddEventButtom'
-import EventForm from '@/components/events/EventForm-old'
+import { EventList } from '@/components/events/EventList'
+import { getEvents } from '@/actions/events'
 
-export default function EventosPage() {
+export default async function EventosPage({ searchParams }) {
+  
+  const { page, pageSize } = searchParams
+  
+  const { data, error } = await getEvents({ page, pageSize })
+
+  if( error ){
+    notFound()
+  }
+
   return (
     <>
       <div className="flex items-center justify-between">
         <h1 className="text-2xl md:text-3xl font-title font-bold">Eventos</h1>
         <AddEventButtom />
       </div>
-      <EventForm />
       <div>
-        Events List
+        <EventList
+          events={ data }
+        />
       </div>
     </>
   )
