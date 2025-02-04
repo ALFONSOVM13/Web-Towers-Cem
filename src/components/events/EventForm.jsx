1f24ea059dda211bdfabebc30bd1a676b6dce709
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-04 14:32:57 -0600

    Crear nuevo evento desde EventForm, implementado

--- src/components/events/EventForm.jsx
+++ src/components/events/EventForm.jsx
@@ -1,45 +1,67 @@
-import React, { useRef, useState } from 'react'
+'use client'
+import React, { useState } from 'react'
 import { MdClose } from 'react-icons/md'
 import { Controller, useForm } from 'react-hook-form'
 import { CKEditorClassic } from '@/components/ui/CKEditorClassic'
-import { UploadImage } from '../ui/UploadImage'
+import { Switch } from '@/components/ui/Switch'
+import { LoadingCircle } from '@/components/ui/LoadingCircle'
+import { ImageSelected } from '@/components/images/ImageSelected'
+import { createEvent } from '@/actions/events'
 import { isValidUrl } from '@/utils/validators'
-import { Switch } from '../ui/Switch'
+import { toastError, toastSuccess } from '@/libs/toast'
 
-export const EventForm = ({ title, onClose }) => {
+export const EventForm = ({ title, onClose, eventEdit }) => {
 
-  const [loading, setLoading] = useState(false)
-  const { register, control, handleSubmit, formState:{ errors }, getValues, setValue, watch } = useForm({
+  const [isLoading, setIsLoading] = useState(false)
+  const { register, control, handleSubmit, formState: { errors }, getValues } = useForm({
     defaultValues: {
       title: '',
       url: '',
-      description: '',
       bannerDesktop: null,
-      bannerDesktopFile: null,
-      bannerMovil: null,
-      bannerMovilFile: null,
-      published: true
+      bannerMobile: null,
+      isPublished: true
     }
   })
 
-  const bannerDesktopRef = useRef({})
-  bannerDesktopRef.current = watch('bannerDesktop', '')
 
-  const handleRemoveBannerDesktop = () => {
-    setValue('bannerDesktop', null, { shouldValidate: true })
+  const handleEventSubmit = async (formData) => {
+
+    const { title, url, bannerDesktop, bannerMobile, isPublished } = formData
+
+    setIsLoading(true)
+    try {
+
+      if (eventEdit) {
+        // Editar evento
+
+      } else {
+        // Crear evento
+        const { error } = await createEvent({
+          title,
+          url,
+          bannerDesktop: bannerDesktop.id,
+          bannerMobile: bannerMobile.id,
+          isPublished
+        })
+
+        if (error) {
+          throw new Error(error)
+        }
+
+        setTimeout(() => {
+          toastSuccess('¡Evento creado correctamente!')
+        }, 100);
+        onClose()
+      }
+
+    } catch (error) {
+      toastError(error.message)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
-  const bannerMovilRef = useRef({})
-  bannerMovilRef.current = watch('bannerMovil', '')
-
-  const handleRemoveBannerMovil = () => {
-    setValue('bannerMovil', null, { shouldValidate: true })
-  }
-
-
-  const handleEventSubmit = ( formData ) => {
-    console.log(formData)
-  }
+  console.log(getValues('isPublished'))
 
   return (
     <div className="w-[94vw] max-h-[97vh] px-4 py-5 sm:px-5 sm:p-5 sm:max-w-[40rem] overflow-y-auto">
@@ -47,13 +69,14 @@ export const EventForm = ({ title, onClose }) => {
         <h3 className="text-xl font-bold">{title}</h3>
         <button
           onClick={onClose}
+          disabled={isLoading}
           className="p-1 border rounded hover:bg-gray-100 text-gray-500 hover:text-gray-800"
         >
           <MdClose />
         </button>
       </header>
-      <form 
-        onSubmit={handleSubmit( handleEventSubmit )}
+      <form
+        onSubmit={handleSubmit(handleEventSubmit)}
         className="flex flex-col gap-3"
       >
         <div>
@@ -68,13 +91,13 @@ export const EventForm = ({ title, onClose }) => {
             id="title"
             placeholder="Título del evento"
             className="block w-full border p-2 rounded focus:outline-blue-800 hover:border-blue-800"
-            { ...register('title',{
+            {...register('title', {
               required: 'El título es requerido',
               validate: value => value.trim() === '' ? 'El título es requerido' : undefined
             })}
           />
-          { errors.title && (
-            <span className="text-sm text-red-600">{ errors.title.message }</span>
+          {errors.title && (
+            <span className="text-sm text-red-600">{errors.title.message}</span>
           )}
         </div>
         <div className="flex flex-col gap-1">
@@ -82,23 +105,22 @@ export const EventForm = ({ title, onClose }) => {
             htmlFor="url-event"
             className="block text-sm sm:text-base font-medium text-gray-600"
           >
-            Url<span className="text-red-600 text-xs align-top">*</span>
+            Url
           </label>
           <input
             type="text"
             id="url-event"
             placeholder="URL del evento"
             className="block w-full border p-2 rounded focus:outline-blue-800 hover:border-blue-800"
-            { ...register('url', {
-              required: 'La URL es requerida',
-              validate: value => !isValidUrl(value) ? 'La URL del evento no es válida' : undefined
+            {...register('url', {
+              validate: value => value && !isValidUrl(value) ? 'La URL del evento no es válida' : undefined
             })}
           />
-          { errors.url && (
-            <span className="text-sm text-red-600">{ errors.url.message }</span>
+          {errors.url && (
+            <span className="text-sm text-red-600">{errors.url.message}</span>
           )}
         </div>
-        <div className="flex flex-col gap-1">
+        {/* <div className="flex flex-col gap-1">
           <label
             htmlFor="event-description"
             className="block text-sm sm:text-base font-medium text-gray-600"
@@ -108,7 +130,7 @@ export const EventForm = ({ title, onClose }) => {
           <Controller
             control={control}
             name="description"
-            render={({field}) => (
+            render={({ field }) => (
               <CKEditorClassic
                 value={field.value}
                 onChanche={(data) => field.onChange(data)}
@@ -117,7 +139,7 @@ export const EventForm = ({ title, onClose }) => {
               />
             )}
           />
-        </div>
+        </div> */}
         <div className="flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-5">
           <div className="sm:flex-1">
             <label
@@ -129,29 +151,25 @@ export const EventForm = ({ title, onClose }) => {
             <div className="h-36 sm:h-52">
               <Controller
                 control={control}
-                name="bannerDesktopFile"
-                render={({field}) => (
-                  <UploadImage
-                    id='banner-desktop'
-                    defaultImage={ getValues('bannerDesktop') }
-                    onRemoveDefaultImage={handleRemoveBannerDesktop}
-                    file={field.value}
+                name="bannerDesktop"
+                render={({ field }) => (
+                  <ImageSelected
+                    value={field.value}
                     onChange={field.onChange}
-                    disabled={loading}
-                    iconSize={10}
-                    object="contain"
+                    defaultImageCategory='2'
+                    object='contain'
                   />
                 )}
                 rules={{
-                  validate: value => (value === null && getValues('bannerDesktop') === null) ? 'Banner del evento para desktop requerido' : undefined
+                  required: 'El Banner de escritorio es requerido'
                 }}
               />
-              { errors.bannerDesktopFile && (
-                <span className="text-sm text-red-600">{ errors.bannerDesktopFile.message }</span>
-              )}
             </div>
+            {errors.bannerDesktop && (
+              <span className="text-sm text-red-600">{errors.bannerDesktop.message}</span>
+            )}
           </div>
-          <div className="sm:w-[10rem]">
+          <div className="sm:w-[13rem]">
             <label
               htmlFor="banner-movil"
               className="inline-block mb-1 text-sm sm:text-base font-medium text-gray-600"
@@ -161,52 +179,65 @@ export const EventForm = ({ title, onClose }) => {
             <div className="h-36 sm:h-52">
               <Controller
                 control={control}
-                name="bannerMovilFile"
-                render={({field}) => (
-                  <UploadImage
-                    id='banner-movil'
-                    defaultImage={ getValues('bannerMovil') }
-                    onRemoveDefaultImage={handleRemoveBannerMovil}
-                    file={field.value}
+                name="bannerMobile"
+                render={({ field }) => (
+                  <ImageSelected
+                    value={field.value}
                     onChange={field.onChange}
-                    disabled={loading}
-                    iconSize={10}
-                    object="contain"
+                    defaultImageCategory='2'
+                    object='contain'
                   />
                 )}
                 rules={{
-                  validate: value => (value === null && getValues('bannerMovil') === null) ? 'Banner del evento para móvil requerido' : undefined
+                  required: 'El Banner Movíl es requerido'
                 }}
               />
-              { errors.bannerMovilFile && (
-                <span className="text-sm text-red-600">{ errors.bannerMovilFile.message }</span>
-              )}
             </div>
+            {errors.bannerMobile && (
+              <span className="text-sm text-red-600">{errors.bannerMobile.message}</span>
+            )}
           </div>
         </div>
         <div className="flex flex-col items-start gap-1 mt-2">
-            <label
-              htmlFor="published-event"
-              className="block text-sm sm:text-base font-medium text-gray-600"
-            >
-              Publicar
-            </label>
-            <Controller
-              name="published"
-              control={control}
-              render={({ field }) => (
-                  <Switch
-                    value={field.value}
-                    onChange={field.onChange}
-                    id="published-event"
-                    disabled={loading}
-                  />
-              )}
-            />
+          <label
+            htmlFor="published-event"
+            className="block text-sm sm:text-base font-medium text-gray-600"
+          >
+            Publicar
+          </label>
+          <Controller
+            name="isPublished"
+            control={control}
+            render={({ field }) => (
+              <Switch
+                value={field.value}
+                onChange={field.onChange}
+                id="published-event"
+                disabled={isLoading}
+              />
+            )}
+          />
         </div>
-        <div className="flex justify-end mt-2">
-          <button className="bg-primary-200 hover:bg-primary-100 text-white px-4 py-2 rounded">
-            { getValues('published') ? 'Publicar' : 'Guardar' }
+
+        <div className="flex flex-col-reverse md:flex-row md:justify-end gap-2 mt-2">
+          <button
+            type="button"
+            onClick={onClose}
+            disabled={isLoading}
+            className="enabled:hover:bg-slate-100 font-semibold text-gray-500 enabled:hover:text-gray-700 text-sm py-2 px-2 rounded-md transition min-w-32"
+          >
+            Cancelar
+          </button>
+          <button
+            type="submit"
+            onClick={() => { }}
+            disabled={isLoading}
+            className="bg-primary-200 text-white enabled:hover:bg-primary-100 disabled:opacity-60 font-semibold text-sm px-4 py-2 rounded transition min-w-32"
+          >
+            {isLoading
+              ? <LoadingCircle className="w-6 h-6" />
+              : getValues('isPublished') ? 'Publicar' : 'Guardar'
+            }
           </button>
         </div>
       </form>
