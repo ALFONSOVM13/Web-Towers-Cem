b8f082f16a58dcb1ec7f24332fcd62a4604be544
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-04 14:45:55 -0600

    label añadido a RegistersCounter

--- src/components/ui/RegistersCounter.jsx
+++ src/components/ui/RegistersCounter.jsx
@@ -1,10 +1,10 @@
 
-export const RegistersCounter = ({ pageSize, currentPage, currentPageSize, totalRegisters }) => {
+export const RegistersCounter = ({ pageSize, currentPage, currentPageSize, totalRegisters, label='registros' }) => {
   return (
     <div className="text-sm text-gray-500 flex items-center gap-1">
       <span>{pageSize * (currentPage - 1) + 1}</span> -
       <span>{pageSize * (currentPage - 1) + currentPageSize}</span>
-      de {totalRegisters} registros
+      de {totalRegisters} {label}
     </div>
   )
 }
