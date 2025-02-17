f0bf66f5b0aea4c1f99eb1d9875080655ac637f4
Tonatiuj Sánchez <tonsanzjimz@gmail.com>
2025-02-16 20:46:06 -0600

    Link para agregar nueva noticias, agregado en NoticiasPage

--- src/app/admin/noticias/page.jsx
+++ src/app/admin/noticias/page.jsx
@@ -1,9 +1,25 @@
 import NewsForm from '@/components/ui/NewsForm'
+import Link from 'next/link'
 import React from 'react'
+import { BiPlusCircle } from 'react-icons/bi'
 
 const page = () => {
   return (
-    <NewsForm/>
+    <>
+      <div className="flex items-center justify-between">
+        <h1 className="text-2xl md:text-3xl font-title font-bold">Noticias</h1>
+        <Link
+          href="/admin/noticias/nuevo"
+          className="flex items-center gap-1 px-3 py-2 font-semibold text-sm bg-primary-200 hover:bg-primary-100 rounded text-white transition"
+        >
+          <BiPlusCircle size={20} />
+          Agregar noticia
+        </Link>
+      </div>
+      <div>
+        <h2>Noticias List</h2>
+      </div>
+    </>
   )
 }
 
